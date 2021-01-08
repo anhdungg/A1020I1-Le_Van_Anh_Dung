b47c2bc7cb0a7e8b38968067f9c427f47a0b41f3
Anhdungg <ledung.ht17@gmail.com>
2021-01-08 22:13:28 +0700

    module 2

---
+++ Module_2/src/introduction_to_java/hien_thi_loi_chao.java
@@ -0,0 +1,12 @@
+package introduction_to_java;
+
+import java.util.Scanner;
+
+public class hien_thi_loi_chao {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        System.out.println("Hello: " + name);
+    }
+}
