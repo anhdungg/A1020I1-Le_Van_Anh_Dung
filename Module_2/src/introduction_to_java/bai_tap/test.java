cced4f97776bb29c66049266ab71e8299f639083
anh dung <ledung.ht17@gmail.com>
2021-01-10 00:22:37 +0700

    Done

--- Module_2/src/introduction_to_java/bai_tap/test.java
+++ Module_2/src/introduction_to_java/bai_tap/test.java
@@ -1,4 +1,25 @@
 package introduction_to_java.bai_tap;
 
+import java.util.Scanner;
+import java.lang.*;
+
 public class test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number ");
+        int number = sc.nextInt();
+        String[] ones = {"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve",
+                "Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+        String[] tens = {"Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+        if(number>0&&number<20){
+            System.out.println(ones[number-1]);
+        }
+        if(number>=20&&number<100){
+            double a = (double) number/10;
+            double b = (a-(int)a)*10;
+            int c = (int)Math.round(b);
+            if (c!=0) System.out.println(tens[(int)a-2] + " " + ones[c-1]);
+            else System.out.println(tens[(int)a-2]);
+        }
+    }
 }
--- Module_2/src/introduction_to_java/bai_tap/ung_dung_doc_so_thanh_chu.java
+++ Module_2/src/introduction_to_java/bai_tap/ung_dung_doc_so_thanh_chu.java
@@ -5,109 +5,28 @@ public class ung_dung_doc_so_thanh_chu {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int numberInt;
+        String[] one = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] two = {"", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+        String[] twos = {"", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
         do {
             System.out.print("Nhập số tối đa 3 chữ số: ");
             String numberString = input.nextLine();
             numberInt = Integer.parseInt(numberString);
             if(numberInt>=100 && numberInt<=999){
-                char[] arrNumber = new char[numberString.length()];
+                int[] arrNumber = new int[numberString.length()];
                 for(int i=0;i<arrNumber.length; i++){
-                    arrNumber[i] = numberString.charAt(i);
+                    arrNumber[i] = (int)numberString.charAt(i)-48;
+                    System.out.println(arrNumber[i]);
                 }
-                String output="";
-                switch (arrNumber[0]){
-                    case '1':
-                        output += "one hundred ";
-                        break;
-                    case '2':
-                        output += "two hundred ";
-                        break;
-                    case '3':
-                        output += "three hundred ";
-                        break;
-                    case '4':
-                        output += "four hundred ";
-                        break;
-                    case '5':
-                        output += "five hundred ";
-                        break;
-                    case '6':
-                        output += "six hundred ";
-                        break;
-                    case '7':
-                        output += "seven hundred ";
-                        break;
-                    case '8':
-                        output += "eight hundred ";
-                        break;
-                    case '9':
-                        output += "nine hundred ";
-                        break;
-                }
-                if (arrNumber[1]=='1'){
-                    switch (arrNumber[2]){
-                        case '0':
-                            output += "ten ";
-                            break;
-                        case '1':
-                            output += "eleven ";
-                            break;
-                        case '2':
-                            output += "twelve ";
-                            break;
-                        case '3':
-                            output += "thirteen ";
-                            break;
-                        case '4':
-                            output += "fourteen ";
-                            break;
-                        case '5':
-                            output += "fifteen ";
-                            break;
-                        case '6':
-                            output += "sixteen ";
-                            break;
-                        case '7':
-                            output += "seventeen ";
-                            break;
-                        case '8':
-                            output += "eighteen ";
-                            break;
-                        case '9':
-                            output += "nineteen ";
-                            break;
-                    }
-                }else if (arrNumber[1]=='0'){
-                    output = getString(arrNumber, output);
-                }
-                else {
-                    switch (arrNumber[1]){
-                        case '2':
-                            output += "twenty ";
-                            break;
-                        case '3':
-                            output += "thirty ";
-                            break;
-                        case '4':
-                            output += "forty ";
-                            break;
-                        case '5':
-                            output += "fifty ";
-                            break;
-                        case '6':
-                            output += "sixty ";
-                            break;
-                        case '7':
-                            output += "seventy ";
-                            break;
-                        case '8':
-                            output += "eighty ";
-                            break;
-                        case '9':
-                            output += "ninety ";
-                            break;
-                    }
-                    output = getString(arrNumber, output);
+                String output = one[arrNumber[0]] + " hundred ";
+                if(arrNumber[2]==0){
+                    output+=twos[arrNumber[1]];
+                }else if(arrNumber[1]==1){
+                    output+=two[arrNumber[2]];
+                }else if(arrNumber[1]==0){
+                    output+=one[arrNumber[2]];
+                }else {
+                    output+=twos[arrNumber[1]] + "-" + one[arrNumber[2]];
                 }
                 System.out.println(output);
             }else {
@@ -115,38 +34,5 @@ public class ung_dung_doc_so_thanh_chu {
             }
         }
         while (numberInt>999 || numberInt<100);
-    }
-
-    private static String getString(char[] arrNumber, String output) {
-        switch (arrNumber[2]){
-            case '1':
-                output += "one ";
-                break;
-            case '2':
-                output += "two ";
-                break;
-            case '3':
-                output += "three ";
-                break;
-            case '4':
-                output += "four ";
-                break;
-            case '5':
-                output += "five ";
-                break;
-            case '6':
-                output += "six ";
-                break;
-            case '7':
-                output += "seven ";
-                break;
-            case '8':
-                output += "eight ";
-                break;
-            case '9':
-                output += "nine ";
-                break;
-        }
-        return output;
     }
 }
