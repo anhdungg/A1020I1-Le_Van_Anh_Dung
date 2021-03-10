19408005bd87a9c446773ed2bcbf66ff1937f141
Anhdungg <ledung.ht17@gmail.com>
2021-03-10 22:04:34 +0700

    Done

--- Module_2/src/xu_ly_ngoai_le_va_debug/bai_tap/su_dung_lop_IllegalTriangleException/CheckTriangle.java
+++ Module_2/src/xu_ly_ngoai_le_va_debug/bai_tap/su_dung_lop_IllegalTriangleException/CheckTriangle.java
@@ -1,4 +1,36 @@
 package xu_ly_ngoai_le_va_debug.bai_tap.su_dung_lop_IllegalTriangleException;
 
+import java.util.Scanner;
+
 public class CheckTriangle {
+
+    private static boolean checkTriangle(int a, int b, int c) throws IllegalTriangleException{
+        if (a<0 && b<0 && c<0){
+            throw new IllegalTriangleException();
+        }else if((a+b)<c){
+            throw new IllegalTriangleException("Hai cạnh phải lớn hơn cạnh còn lại.");
+        } else {
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhập cạnh thứ nhất: ");
+        int a = input.nextInt();
+        input.nextLine();
+        System.out.print("Nhập cạnh thứ hai: ");
+        int b = input.nextInt();
+        input.nextLine();
+        System.out.print("Nhập cạnh thứ ba: ");
+        int c = input.nextInt();
+        input.nextLine();
+        try {
+            if(checkTriangle(a,b,c)){
+                System.out.println("Nhập tam giác thành công.");
+            }
+        }catch (IllegalTriangleException e){
+            e.printStackTrace();
+        }
+    }
 }
--- Module_2/src/xu_ly_ngoai_le_va_debug/bai_tap/su_dung_lop_IllegalTriangleException/IllegalTriangleException.java
+++ Module_2/src/xu_ly_ngoai_le_va_debug/bai_tap/su_dung_lop_IllegalTriangleException/IllegalTriangleException.java
@@ -1,4 +1,12 @@
 package xu_ly_ngoai_le_va_debug.bai_tap.su_dung_lop_IllegalTriangleException;
 
-public class IllegalTriangleException {
+public class IllegalTriangleException extends Exception{
+
+    public IllegalTriangleException(){
+        super("Kiểm tra lại tam giác!!!!");
+    }
+
+    public IllegalTriangleException(String str){
+        super(str);
+    }
 }
