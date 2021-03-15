535c4a1524240cce4e0a66f002fe8c33a5aad87c
Anhdungg <ledung.ht17@gmail.com>
2021-03-15 22:31:27 +0700

    Done

--- Module_2/src/String_va_Regex/bai_tap/Validate_so_dien_thoai/ValidatePhoneNumber.java
+++ Module_2/src/String_va_Regex/bai_tap/Validate_so_dien_thoai/ValidatePhoneNumber.java
@@ -1,4 +1,17 @@
 package String_va_Regex.bai_tap.Validate_so_dien_thoai;
 
+import java.util.Scanner;
+
 public class ValidatePhoneNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String check = "(\\(84\\)-)\\([0][\\d]{9}\\)";
+        System.out.print("Nhập số điện thoại: ");
+        String str = input.nextLine();
+        if (str.matches(check)){
+            System.out.println("Số điện thoại hợp lệ.");
+        }else {
+            System.out.println("Số điện thoại không hợp lệ.");
+        }
+    }
 }
--- Module_2/src/String_va_Regex/bai_tap/Validate_ten_cua_lop_hoc/ValidateNameClass.java
+++ Module_2/src/String_va_Regex/bai_tap/Validate_ten_cua_lop_hoc/ValidateNameClass.java
@@ -1,4 +1,17 @@
 package String_va_Regex.bai_tap.Validate_ten_cua_lop_hoc;
 
+import java.util.Scanner;
+
 public class ValidateNameClass {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String check = "[CAP][0-9]{4}[GHIKLM]";
+        System.out.print("Nhập tên lớp học: ");
+        String str = input.nextLine();
+        if (str.matches(check)){
+            System.out.println("Tên lớp hợp lệ.");
+        }else {
+            System.out.println("Tên lớp không hợp lệ.");
+        }
+    }
 }
