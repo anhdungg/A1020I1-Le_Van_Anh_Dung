0b59001c0c0b644f7103ef9b469c7dd9672443db
anh dung <ledung.ht17@gmail.com>
2021-04-03 00:23:23 +0700

    Done

--- Module_3/01_thiet_ke_va_tao_CSDL/bai_tap/01_phan_tich_thiet_ke_CSDL/phan_tich_thiet_ke_CSDL.sql
+++ Module_3/01_thiet_ke_va_tao_CSDL/bai_tap/01_phan_tich_thiet_ke_CSDL/phan_tich_thiet_ke_CSDL.sql
@@ -4,34 +4,90 @@ create database phan_tich_va_thiet_ke_CSDL;
 
 use phan_tich_va_thiet_ke_CSDL;
 
-create table customers(
-	id int auto_increment primary key,
-    `name` varchar(40),
-    phone_number int(11),
-    address varchar(255)
+create table productlines(
+	productline varchar(50) primary key,
+    text_descriptions text,
+    image char
 );
 
-create table products_line(
-	id int,
-    descriptions varchar(255)
+create table offices(
+	office_code varchar(10) primary key,
+    city varchar(50) not null,
+    phone varchar(50) not null,
+    address_line_1 varchar(50) not null,
+    address_line_2 varchar(50),
+    state varchar(50),
+    country varchar(50) not null, 
+    postal_code varchar(15) not null
+);
+
+create table employees(
+	employee_number int primary key,
+    last_name varchar(50) not null,
+    first_name varchar(50) not null,
+    email varchar(100) not null,
+	job_title varchar(50) not null,
+    manage int,
+    office_code varchar(10),
+    -- foreign key(employee_number) references employees(manage),
+    foreign key(office_code) references offices(office_code)
+);
+
+create table customers(
+	customer_number int primary key,
+    customer_name varchar(50) not null,
+    contact_last_name varchar(50) not null,
+    contact_first_name varchar(50) not null,
+    phone_number varchar(50) not null,
+    address_line1 varchar(50) not null,
+    address_line2 varchar(50),
+    city varchar(50)not null,
+    state varchar(50)not null,
+    postal_code varchar(15)not null,
+    country varchar(50)not null,
+    credit_limit float,
+    employee_number int,
+    foreign key(employee_number) references employees(employee_number)
 );
 
 create table products(
-	id int primary key,
-    `name` varchar(40),
-    supplier varchar(40),
-    number_stock int,
-    price_entered int,
-    price int
+	product_code varchar(15) primary key,
+    product_name varchar(70) not null,
+    product_scale varchar(10) not null,
+    product_vendor varchar(50) not null,
+    product_description text not null,
+    quanity_in_stock int not null,
+    buy_price float not null,
+    MSRP float not null,
+    productline varchar(50),
+    foreign key(productline) references productlines(productline)
 );
 
 create table orders(
-	order_number int,
-    date_of_purchase date,
-    delivery_date date,
-    actual_delivery_date date,
-    number_of_order int,
-    price int
+	order_number int primary key,
+    order_date date not null,
+    required_date date not null,
+    shipped_date date,
+    `status` varchar(15) not null,
+    `comment` text,
+    quantity_ordered int not null,
+    price_each float not null,
+    customer_number int,
+    foreign key(customer_number) references customers(customer_number)
 );
 
--- create table 
+create table payments(
+	check_number varchar(50) primary key,
+    payment_date date not null,
+    amount float not null,
+    customer_number int,
+    foreign key(customer_number) references customers(customer_number)
+);
+
+create table product_order(
+	product_code varchar(15),
+    order_number int,
+    primary key(product_code, order_number),
+    foreign key(product_code) references products(product_code),
+    foreign key(order_number) references orders(order_number)
+);
