60af4c2f4a9a32fd7df264a431101fe54026267c
anh dung <ledung.ht17@gmail.com>
2021-07-20 10:58:28 +0700

    Update using session

--- Module_4/09_Session_va_Cookie_trong_Spring/Bai_tap/tao_gio_hang/src/main/java/vn/codegym/repository/CartRepository.java
+++ Module_4/09_Session_va_Cookie_trong_Spring/Bai_tap/tao_gio_hang/src/main/java/vn/codegym/repository/CartRepository.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface CartRepository {
     List<Product> findAll();
+    
     Product findById(int id);
+
     void save(Product product);
+
     void delete(int id);
 }
